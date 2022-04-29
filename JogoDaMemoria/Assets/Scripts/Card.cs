1febb85e21c7a30d6883f6346c51d7092ef40181
JHDsBR <Jesse2013.jsc@gmail.com>
2022-04-28 22:30:55 -0300

    Ganhar e perder
    O jogo já é capaz se saber se virou 2 cartas certas ou não

--- JogoDaMemoria/Assets/Scripts/Card.cs
+++ JogoDaMemoria/Assets/Scripts/Card.cs
@@ -7,27 +7,46 @@ public class Card : MonoBehaviour
 {
     public SpriteRenderer frontIcon;
     public Animator cardAnimator;
+    public CardManager CM; // o própio CardManager que define essa variável
+
+    private bool imFacedUp;
 
     // Start is called before the first frame update
     void Start()
     {
         if(!cardAnimator)
-        {
             cardAnimator = GetComponent<Animator>();
-        }
+
+        gameObject.name = "Card "+frontIcon.sprite.name; // muda o nome do gameObject, é através desse nome que é feito a comparação dos cards
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     void OnMouseDown()
     {
         // Debug.Log("You clicked me");
+
+        if(imFacedUp || !CM.canFlip)
+            return;
+
+        FlipIn();
+        CM.PickCard(gameObject);
+    }
+
+
+    public void FlipIn()
+    {
+        imFacedUp = true;
         cardAnimator.SetBool("FlipIn", true);
+        cardAnimator.SetBool("FlipOut", false);
+    }
+
+
+    public void FlipOut()
+    {
+        imFacedUp = false;
+        cardAnimator.SetBool("FlipOut", true);
+        cardAnimator.SetBool("FlipIn", false);
     }
 
 }
--- JogoDaMemoria/Assets/Scripts/CardManager.cs
+++ JogoDaMemoria/Assets/Scripts/CardManager.cs
@@ -4,25 +4,57 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    public int numberOfCards=18;
     public GameObject cardModel;
     public string sheetname;
     public GameObject cardGrid;
-    private Sprite[] sprites;
+
+    private List<Sprite> sprites;
     private SpriteRenderer sr;
     private string[] names;
 
+    private List<GameObject> lastTwoCardFaceUp = new List<GameObject>();
+
+
+    public bool canFlip=true;
+
     void Start ()
     {
-        sprites = Resources.LoadAll<Sprite>(sheetname);
-        // sr = GetComponent<SpriteRenderer>();
-        names = new string[sprites.Length];
+        CreateCards();
+        StartCoroutine(CompareCards());
+    }
 
-        foreach(Sprite spt in sprites)
+
+    IEnumerator CompareCards()
+    {
+        while(true)
         {
-            // print(sprites[i].name);
-            GameObject cardTmp = Instantiate(cardModel, cardGrid.transform) as GameObject;
-            cardTmp.GetComponent<Card>().frontIcon.sprite = spt;
-            // names[i] = sprites[i].name;
+
+            if(lastTwoCardFaceUp.Count == 2)
+            {
+                canFlip = false;
+                Animator lastAnimator = lastTwoCardFaceUp[1].GetComponent<Animator>();
+
+                yield return new WaitUntil(() => (lastAnimator.GetCurrentAnimatorStateInfo(0).IsName("CardFlip") && lastAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime>=1));
+
+                if(lastTwoCardFaceUp[0].name == lastTwoCardFaceUp[1].name)
+                {
+                    print("Acertou");
+                }
+                else
+                {
+                    print("Errou");
+                    yield return new WaitForSeconds(0.5f);
+                    lastTwoCardFaceUp[0].GetComponent<Card>().FlipOut();
+                    yield return new WaitForSeconds(0.3f);
+                    lastTwoCardFaceUp[1].GetComponent<Card>().FlipOut();
+                }
+
+                lastTwoCardFaceUp = new List<GameObject>();
+                canFlip = true;
+            }
+
+            yield return null;
         }
     }
 
@@ -30,12 +62,58 @@ public class CardManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
 
     void CreateCards()
     {
+        sprites = new List<Sprite>(Resources.LoadAll<Sprite>(sheetname));
+        names = new string[sprites.Count];
 
+        for(int c=0; c<numberOfCards; c+=2)
+        {
+            Sprite spt = sprites[Random.Range(0, sprites.Count)];
+            Color cor = new Color(Random.value,Random.value,Random.value);
+
+            GameObject cardTmp = Instantiate(cardModel, cardGrid.transform) as GameObject;
+            cardTmp.GetComponent<SpriteRenderer>().color = cor;
+            cardTmp.GetComponent<Card>().frontIcon.sprite = spt;
+            cardTmp.GetComponent<Card>().CM = this;
+
+            cardTmp = Instantiate(cardModel, cardGrid.transform) as GameObject;
+            cardTmp.GetComponent<SpriteRenderer>().color = cor;
+            cardTmp.GetComponent<Card>().frontIcon.sprite = spt;
+            cardTmp.GetComponent<Card>().CM = this;
+
+            sprites.Remove(spt);
+        }
+
+        SortCards();
     }
+
+    public void PickCard(GameObject card)
+    {
+        lastTwoCardFaceUp.Add(card);
+    }
+
+
+    void SortCards()
+    {
+        int[] newIndexs = new int[numberOfCards];
+
+        for(int c=0; c<numberOfCards; c++)
+        {
+            newIndexs[c] = Random.Range(0, numberOfCards);
+        }
+
+        int cc=0;
+        foreach(int idx in newIndexs)
+        {
+            Transform child = cardGrid.transform.GetChild(cc);
+            child.SetSiblingIndex(idx);
+            cc++;
+        }
+    }
+
+
 }
